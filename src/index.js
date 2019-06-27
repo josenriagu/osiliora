77e2faa148efc8a0e6a78d3394db322b3d9ef2e1
Josemaria <josemarianriagu25@gmail.com>
2019-06-27 16:36:38 +0800

    Fix ReduxdevTools extension conflict Change site title and manifest name

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -15,9 +16,8 @@ const persistedState = loadState();
 const store = createStore(
     rootReducer,
     persistedState,
-    compose(
+    composeWithDevTools(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
 
